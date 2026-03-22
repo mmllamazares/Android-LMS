bd568ae0e676d001c55ab6158f31aee9f8b08d15
mllamazares <mmllamazares@gmail.com>
2026-03-22 19:21:37 -0400

    add questions

--- app/src/main/java/com/example/myapplication/quizModule/QuizRepository.kt
+++ app/src/main/java/com/example/myapplication/quizModule/QuizRepository.kt
@@ -3,10 +3,11 @@ package com.example.myapplication.quizModule
 class QuizRepository {
     // get the questions from the web, or from db, cloud database
     private val questions = listOf(
-        Question("The Great Wall of China is visible from space with the naked eye", false),
-        Question("Water boils at 100°C at sea level.", true),
-        Question("Humans have more than five senses.", true),
-        Question("Lightning never strikes the same place twice.", false)
+        Question("En dientes anteriores, la entrada a la cavidad endodóntica se realiza por la cara vestibular o bucal.", false),
+        Question("Durante el acceso cameral, la cavidad debe corresponder en su forma a la forma de la cámara pulpar.", true),
+        Question("En molares mandibulares, el triángulo de acceso cameral tiene su base hacia distal y su vértice hacia mesial.", false),
+        Question("La instrumentación de los conductos radiculares debe llegar hasta la constricción apical, que en la mayoría de los casos coincide con el límite cemento-dentinario.", true),
+        Question("En dientes jóvenes con pulpa grande, el acceso cameral en incisivos tiene una forma ovoide, mientras que en dientes adultos es triangular.", false)
     )
 
     fun getAllQuestions():List<Question> = questions
